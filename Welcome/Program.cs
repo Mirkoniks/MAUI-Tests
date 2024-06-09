10313433af762c54131bf14417e8b8023bb66a13
Miroslav Nikolov <mirro.nikolov@gmail.com>
2024-06-09 12:26:00 +0300

    Create MAUI App

--- Welcome/Program.cs
+++ Welcome/Program.cs
@@ -1,13 +1,13 @@
-﻿using Welcome.Model;
-using Welcome.Others;
-using Welcome.View;
-using Welcome.ViewModel;
+﻿//using Welcome.Model;
+//using Welcome.Others;
+//using Welcome.View;
+//using Welcome.ViewModel;
 
-User user = new User("Pesho Goshov", "Parola", UserRoleEnum.STUDENT, "455050965", "emaail@emai.bg", DateTime.UtcNow);
+//User user = new User("Pesho Goshov", "Parola", UserRoleEnum.STUDENT, "455050965", "emaail@emai.bg", DateTime.UtcNow);
 
-UserViewModel viewModel = new UserViewModel(user);
+//UserViewModel viewModel = new UserViewModel(user);
 
-UserView view = new UserView(viewModel);
+//UserView view = new UserView(viewModel);
 
-view.DisplayFull();
+//view.DisplayFull();
 
